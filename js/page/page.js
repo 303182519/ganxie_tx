18cfbcb88afd677890610aaca182566a808596ea
303182519 <303182519@qq.com>
2013-11-18 19:14:22 +0800

    修改

--- js/page/page.js
+++ js/page/page.js
@@ -1,81 +1,144 @@
-var pages={};
+/**
+ * 分页组件
+ * User: xiejinlong@yy.com
+ * Time: (2013-10-28 15:19)
+ */
+(function() {
+    "use strict";
+    /**
+     * Constructs page objects
+     * @class page
+     * @constructor
+     * @example
+     * new page({
+     *       "id_dom":"pagination",
+     *       "total":60,
+     *       "per_page":5,
+     *       "nowpage":2,
+     *       "callback":aa
+     *   });
+     */
+    function page(){
+        this.initialize.apply(this, arguments)
+    }
 
-pages.util=(function(){
-    var distance;
-
-    function constructor(totle,nowpage,id_dom){
-        this.totle=totle;
-        this.nowpage=nowpage;
-        this.htmlStr=[];
-        this.id_dom=document.getElementById(id_dom);
-        this.createHtml();
-        }
-
-    constructor.prototype={
+    page.prototype={
+        /**
+         * 焦点图组件初始化函数
+         * @method initialize
+         * @param {Object} 参数
+         */
+        initialize:function(obj){
+            this.totle=Math.ceil(obj.total/obj.per_page);
+            this.nowpage=obj.nowpage;
+            this.id_dom=document.getElementById(obj.id_dom);
+            this.htmlStr=[];
+            this.callback=obj.callback || function(){};
+            this.createHtml();
+            this.bindEvent();
+        },
         createHtml:function(){
-            //如果当前页数大于1的话，就出现上一页
-            if(this.nowpage>1){
-                this.htmlStr.push('<span href="#'+(this.nowpage-1)+'">上一页</span>');
-            }
+             this.htmlStr.length=0;
+             if(this.totle<1 || this.nowpage> this.totle ){ return false;}
 
-            //如果当前页数为4的话，前面就出现..
-            if(this.nowpage>=4){
-                this.htmlStr.push('<span>..</span>');
+            //如果当前页数>1的话
+            this.nowpage>1 ? this.htmlStr.push('<a href="#" class="prev">上一页</a>') : this.htmlStr.push('<span class="current prev">上一页</span>');
 
-                if((this.totle-this.nowpage)<2){
-                    var pages=this.totle-4;
-                    for(var i=pages; i<=this.totle; i++){
-                        if(i==this.nowpage){
-                             this.htmlStr.push('<span href="#'+i+'" class="on">'+i+'</span>');
-                        }else{
-                            this.htmlStr.push('<span href="#'+i+'">'+i+'</span>');
-                        }
-                    }
-                }else{
-                    for(var i=1; i<=5; i++){
-                        var pages=this.nowpage-3+i;
-                        if(i==3){
-                            this.htmlStr.push('<span href="#'+pages+'" class="on">'+pages+'</span>');
-                        }else{
-                            this.htmlStr.push('<span href="#'+pages+'">'+pages+'</span>');
-                        }
-                    }
-                }
+            if(this.totle<=7){
+                //如果总页数<=7的话
+                this.for_page(1,7);
+
             }else{
+                //如果总页数>8的话
+                //如果当前页数<5
+                if(this.nowpage<5){
+                    this.nowpage<4 ? this.for_page(1,4) : this.for_page(1,5);
+                    this.htmlStr.push('<span>...</span>');
+                    this.htmlStr.push('<a href="#">'+this.totle+'</a>');
+                }else{
+                    this.htmlStr.push('<a href="#">1</a>');
+                    this.htmlStr.push('<span>...</span>');
 
-                for(var i=1; i<=5; i++){
-                    if(this.nowpage==i){
-                        this.htmlStr.push('<span href="#'+i+'" class="on">'+i+'</span>');
+                    if(this.totle-this.nowpage>2){
+
+                        this.for_page(this.nowpage-2,this.nowpage+1);
+
+                        this.htmlStr.push('<span>...</span>');
+                        this.htmlStr.push('<a href="#">'+this.totle+'</a>');
                     }else{
-                        this.htmlStr.push('<span href="#'+i+'">'+i+'</span>');
+                        this.for_page(this.nowpage-2,this.totle);
                     }
                 }
             }
-            //如果总页数大于6，就出现最后一页和..
-            if(this.totle>6 && (this.totle-this.nowpage)>2){
-                this.htmlStr.push('<span>..</span>');
-                this.htmlStr.push('<span href="#'+this.totle+'">'+this.totle+'</span>')
-            }
-
-            //如果当前页数小于总页数的话，就出现下一页
-            if(this.nowpage<this.totle){
-                this.htmlStr.push('<span href="#'+(this.nowpage+1)+'">下一页</span>');
-            }
-
+            //如果当前页数<总页数的话
+            this.nowpage<this.totle ? this.htmlStr.push('<a href="#" class="next">下一页</a>') : this.htmlStr.push('<span class="current next">下一页</span>');
+            //设置html的值
             this.id_dom.innerHTML=this.htmlStr.join("");
-        }
-    }
+        },
+        /**
+         * 循环函数
+         * @method for_page
+         * @param {number} start  开始点
+         * @param {number} end  结束点
+        */
+        for_page:function(start,end){
 
-    return {
-        init:function(totle,nowpage,id_dom){
-            if(!distance){
-                distance=new constructor(totle,nowpage,id_dom);
+            for(var i=start; i<=end; i++){
+                if(i==this.nowpage){
+                    this.htmlStr.push('<span class="current">'+i+'</span>');
+                }else{
+                    this.htmlStr.push('<a href="#">'+i+'</a>');
+                }
+            }
+        },
+        /**
+         * 点击事件
+         * @method bindEvent
+         */
+        bindEvent:function(){
+            var _this=this;
+            this.id_dom.onclick=function(e){
+                var e = e || event,
+                    oTarget = e.target || e.srcElement;
+                if(_this.contains(oTarget, this) && _this.isParent(oTarget, "a")) {
+                    if(oTarget.innerHTML=="上一页"){
+                        _this.nowpage=_this.nowpage-1;
+                    }else if(oTarget.innerHTML=="下一页"){
+                        _this.nowpage=_this.nowpage+1;
+                    }else {
+                        _this.nowpage=parseInt(oTarget.innerHTML);
+                    }
+
+                    _this.createHtml();
+                    _this.callback(_this.nowpage);
+                }
             }
 
-            return distance;
+        },
+        contains:function(element,oParent){
+            if(oParent.contains) {
+                return oParent.contains(element)
+            }
+            else if(oParent.compareDocumentPosition) {
+                return !!(oParent.compareDocumentPosition(element) & 16)
+            }
+        },
+        isParent:function(element, tagName) {
+            while(element != undefined && element != null && element.tagName.toUpperCase() !== "BODY") {
+                if(element.tagName.toUpperCase() == tagName.toUpperCase())
+                    return element;
+                element = element.parentNode;
+            }
+            return false
         }
+
     }
 
-})();
-
-pages.util.init(25,6,"page");
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = page;
+    } else if (typeof define === 'function' && define.cmd) {
+        define(function(){return page;});
+    } else {
+        (function(){ return this || (0,eval)('this'); }()).page = page;
+    }
+}());
